ccc6e98e31480e1e7446d0aa69b3f69f6740c390
Radu Micu <radu.micu@gmail.com>
2019-03-27 15:18:04 +0000

    Fix npm module files, add more rules

--- rules/stylistic-issues.js
+++ rules/stylistic-issues.js
@@ -55,6 +55,14 @@ module.exports = {
 			ignore: ["after-comment"]
 		}
 	],
+	"media-feature-colon-space-after": "always",
+	"media-feature-colon-space-before": "never",
+	"media-feature-name-case": "lower",
+	"media-feature-parentheses-space-inside": "never",
+	"media-feature-range-operator-space-after": "always",
+	"media-feature-range-operator-space-before": "always",
+	"media-query-list-comma-newline-after": "always-multi-line",
+	"media-query-list-comma-newline-before": "never-multi-line",
 	"at-rule-empty-line-before":[
 		"always",
 		{
