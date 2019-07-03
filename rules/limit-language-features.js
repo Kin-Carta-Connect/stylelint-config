42c330960f39be0301179ca2bd33cd7afffce17e
Radu Micu <radu.micu@gmail.com>
2019-07-03 10:39:59 +0100

    Disable selector-class-pattern for now to fix #33

--- rules/limit-language-features.js
+++ rules/limit-language-features.js
@@ -6,14 +6,16 @@ module.exports = {
 	"declaration-block-no-redundant-longhand-properties": true,
 	"declaration-no-important": true,
 	"declaration-block-single-line-max-declarations": 1,
-	"selector-class-pattern": [
-		"^(--)?([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
-		{
-			resolveNestedSelectors: true,
-			message:
-				"Expect class selector to conform to AR BEM flavour, see styleguide docs (regexr.com/4b2ts tests). Example: `listing-card --featured`"
-		}
-	],
+	// Disabled because it doesn't work with Sass composed selectors
+	// https://gitlab.com/amazerealise-fe-group/fe-boilerplate/issues/33
+	// "selector-class-pattern": [
+	// 	"^(--)?([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+	// 	{
+	// 		resolveNestedSelectors: true,
+	// 		message:
+	// 			"Expect class selector to conform to AR BEM flavour, see styleguide docs (regexr.com/4b2ts tests). Example: `listing-card --featured`"
+	// 	}
+	// ],
 	"selector-max-empty-lines": 0,
 	"selector-max-compound-selectors": 4,
 	"selector-max-specificity": "0,4,1",
